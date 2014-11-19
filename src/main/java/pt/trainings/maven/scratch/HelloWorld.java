7f26cd96158cf5b2d8fb9e3d4ff143d953f3d0dc
jereksel <jereksel@gmail.com>
2014-11-20 00:18:06 +0100

    Initial commit

---
+++ src/main/java/pt/trainings/maven/scratch/HelloWorld.java
@@ -0,0 +1,11 @@
+package pt.trainings.maven.scratch;
+
+public class HelloWorld {
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        Greetings greetings = new Greetings();
+        System.out.println(greetings.sayHello(args.length>0?args[0]:null));
+    }
+}
